c18955062be712572974514fd61040adc1851b58
Sthe Soares <sthedeveloper@gmail.com>
2017-08-18 11:20:16 -0300

    Cadastro de Produtos - Sqlite
    Curso Android - Crud Completo
    https://www.udemy.com/curso-android-criando-crud-completo/

---
+++ app/src/main/java/projetocadastro/projetocurso/com/cadastroprodutos/ProdutosActivity.java
@@ -0,0 +1,13 @@
+package projetocadastro.projetocurso.com.cadastroprodutos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos);
+    }
+}
